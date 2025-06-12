4760b949969e0e65198486e15c8d91e345233464
Firizqi Aditya Mulya <adityamulyaf@gmail.com>
2025-06-12 14:46:07 +0700

    Update TSP_Recursion.java

--- TSP_Recursion.java
+++ TSP_Recursion.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class TSP_Recursion {
 
-    static int[][] memo;
+    static double[][] memo;
     static int[][] bestPath;
     static String[] cityNames;
 
-    static int INF = (int) 1e9;
+    static double INF = Double.POSITIVE_INFINITY;
 
-    static int totalCost(int mask, int pos, int n, int[][] cost) {
+    static double totalCost(int mask, int pos, int n, double[][] cost) {
         if (mask == (1 << n) - 1) {
             return cost[pos][0] == INF ? INF : cost[pos][0];
         }
 
         if (memo[pos][mask] != -1) return memo[pos][mask];
 
-        int ans = INF;
+        double ans = INF;
         int nextCity = -1;
 
         for (int i = 0; i < n; i++) {
             if ((mask & (1 << i)) == 0 && cost[pos][i] != INF) {
                 int newMask = mask | (1 << i);
-                int temp = cost[pos][i] + totalCost(newMask, i, n, cost);
+                double temp = cost[pos][i] + totalCost(newMask, i, n, cost);
                 if (temp < ans) {
                     ans = temp;
                     nextCity = i;
@@ -54,17 +54,17 @@ public class TSP_Recursion {
     }
 
     public static void main(String[] args) {
-        int INF = (int) 1e9;
+        double INF = Double.POSITIVE_INFINITY;
 
-        int[][] cost = {
-            // PG,   RB,   RJ,   IS,   KS,   ISu,  TA
-            {  0,   41,   74,   27, INF, INF, INF }, // Pasar Gede
-            { 41,    0,   86, INF,  79, INF, INF }, // Rendra Buah
-            { 74,   86,    0,  57, 110, 110,  91 }, // Raja Buah
-            { 27, INF,   57,   0, INF, 112, INF },  // Istana Buah Srikandi
-            {INF,  79,  110, INF,   0, INF,  82 },  // Kios Buah Segar
-            {INF, INF,  110, 112, INF,   0, 183 },  // Istana Buah Sukoharjo
-            {INF, INF,   91, INF,  82, 183,   0 }   // Toko Buah ABC Karanganyar
+        double[][] cost = {
+            // PG,   RB,   RJ,   IS,   KS,   ISu,   TA
+            {  0, 4.1, 7.4, 2.7, INF, INF, INF },   //y Pasar Gede
+            {4.1,   0, 8.6, INF, 7.9, INF, INF },   //y Rendra Buah
+            {7.4, 8.6,   0, 5.7, 11.0, 11.0, 9.1 }, //y Raja Buah
+            {2.7, INF, 5.7,   0, INF, 11.2, INF },  //y Istana Buah Srikandi
+            {INF, 7.9, 11.0, INF, 0, INF, 8.2 },    //y Kios Buah Segar
+            {INF, INF, 11.0, 11.2, INF, 0, 18.3 },  //y Istana Buah Sukoharjo
+            {INF, INF, 9.1, INF, 8.2, 18.3, 0 }     //y Toko Buah ABC Karanganyar
         };
 
         cityNames = new String[] {
@@ -74,27 +74,30 @@ public class TSP_Recursion {
         };
 
         int n = cost.length;
-        memo = new int[n][1 << n];
+        memo = new double[n][1 << n];
         bestPath = new int[n][1 << n];
-        for (int[] row : memo) Arrays.fill(row, -1);
-        for (int[] row : bestPath) Arrays.fill(row, -1);
 
-        int resultCost = totalCost(1, 0, n, cost);
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], -1);
+            Arrays.fill(bestPath[i], -1);
+        }
+
+        double resultCost = totalCost(1, 0, n, cost);
         List<Integer> route = reconstructPath(n);
 
-        System.out.println("Total cost: " + resultCost);
+        System.out.printf("Total cost: %.2f km\n", resultCost);
         System.out.println("Route taken (index): " + route);
         System.out.print("Route taken (named): ");
         for (int i = 0; i < route.size(); i++) {
             System.out.print(cityNames[route.get(i)]);
-            if (i != route.size() - 1) System.out.print(" → ");
+            if (i != route.size() - 1) System.out.print(" - ");
         }
 
         System.out.println("\n\nDetail perjalanan:");
         for (int i = 0; i < route.size() - 1; i++) {
             int a = route.get(i);
             int b = route.get(i + 1);
-            System.out.println(cityNames[a] + " - " + cost[a][b] + " km - " + cityNames[b]);
+            System.out.printf("%s - %.2f km - %s\n", cityNames[a], cost[a][b], cityNames[b]);
         }
     }
 }
